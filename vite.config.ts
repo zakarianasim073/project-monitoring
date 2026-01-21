4833ee844cbcb9ab57eaa0b517d4c56965a04e8a
zakarianasim073 <128282115+zakarianasim073@users.noreply.github.com>
2026-01-22 05:31:31 +0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig(({ mode }) => {
       plugins: [react()],
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
+        'process.env.GEMINI_API_KEY': JSON.stringify(AIzaSyD9EBDj6_hLDjnE0ypLInERfoLPS-Pt-NA)
       },
       resolve: {
         alias: {
