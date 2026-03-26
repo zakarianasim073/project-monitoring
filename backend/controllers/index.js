0003c7880ff898684e06a1f794bc4c6cc48e16ea
zakarianasim073 <128282115+zakarianasim073@users.noreply.github.com>
2026-03-26 17:38:25 +0600

    Create controllers/index.js

---
+++ backend/controllers/index.js
@@ -0,0 +1,8 @@
+// Controller functions and logic go here
+
+module.exports = {
+    // Example controller function
+    exampleFunction: (req, res) => {
+        res.send('Hello from the controller!');
+    }
+};
