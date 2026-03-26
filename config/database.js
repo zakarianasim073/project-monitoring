f92cafed746b4b8b21110af02950c32f5783c594
zakarianasim073 <128282115+zakarianasim073@users.noreply.github.com>
2026-03-26 17:40:31 +0600

    Add MongoDB connection configuration

---
+++ config/database.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGODB_URI, { 
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+        console.log('MongoDB connected...');
+    } catch (error) {
+        console.error(error.message);
+        process.exit(1);
+    }
+};
+
+module.exports = connectDB;
